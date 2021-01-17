088b49b06b242984a79ad60c4bb06efa7858f54c
hanna sonnenberg <sonnenberghanna@googlemail.com>
2021-01-17 15:33:23 +0100

    added before_action investigators and scenarios controller

--- app/controllers/investigators_controller.rb
+++ app/controllers/investigators_controller.rb
@@ -1,18 +1,21 @@
 class InvestigatorsController < ApplicationController
   before_action :set_campaign, only: [:index]
+  before_action :set_investigator, only: [:show]
 
   def index
     @investigators = Investigator.where(campaign: @campaign)
-    # @investigators = Investigator.all
   end
 
   def show
-    @investigator = Investigator.find(params[:id])
   end
 
   private
 
   def set_campaign
     @campaign = Campaign.find(params[:campaign_id])
+  end
+
+  def set_investigator
+    @investigator = Investigator.find(params[:id])
   end
 end
--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,17 +1,21 @@
 class ScenariosController < ApplicationController
   before_action :set_campaign, only: [:index]
+  before_action :set_scenarios, only: [:show]
 
   def index
     @scenarios = Scenario.where(campaign: @campaign)
   end
 
   def show
-    @scenario = Scenario.find(params[:id])
   end
 
   private
 
   def set_campaign
     @campaign = Campaign.find(params[:campaign_id])
+  end
+
+  def set_scenarios
+    @scenario = Scenario.find(params[:id])
   end
 end

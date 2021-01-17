78589d97b2e71e5e75a2e19faaae260f022460a2
hanna sonnenberg <sonnenberghanna@googlemail.com>
2021-01-17 15:19:52 +0100

    added before action campaigns controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,9 +1,16 @@
 class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show]
+
   def index
     @campaigns = Campaign.all
   end
 
   def show
+  end
+
+  private
+
+  def set_campaign
     @campaign = Campaign.find(params[:id])
   end
 end

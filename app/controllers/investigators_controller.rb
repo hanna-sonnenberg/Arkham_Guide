ad3d639bc48c2d4a39539846e192c12bbbeeafbc
hanna sonnenberg <sonnenberghanna@googlemail.com>
2021-02-21 17:19:20 +0100

    new and create action in investigator controller and edit validations for mental, physical trauma, unspent exper points

--- app/controllers/investigators_controller.rb
+++ app/controllers/investigators_controller.rb
@@ -1,5 +1,5 @@
 class InvestigatorsController < ApplicationController
-  before_action :set_campaign, only: [:index]
+  before_action :set_campaign, only: [:index, :new]
   before_action :set_investigator, only: [:show]
 
   def index
@@ -7,6 +7,22 @@ class InvestigatorsController < ApplicationController
   end
 
   def show
+  end
+
+  def new
+    @investigator = Investigator.new
+  end
+
+  def create
+    @investigator = Investigator.new(investigator_params)
+    # we need `campaign_id` to associate investigator with corresponding campaign
+    @campaign = Campaign.find(params[:campaign_id])
+    @investigator.campaign = @campaign
+    if @investigator.save
+      redirect_to campaign_path(@campaign)
+    else
+      render :new
+    end
   end
 
   private
@@ -17,5 +33,9 @@ class InvestigatorsController < ApplicationController
 
   def set_investigator
     @investigator = Investigator.find(params[:id])
+  end
+
+  def investigator_params
+    params.require(:investigator).permit(:name, :player_name, :deck_link, :physical_trauma, :mental_trauma, :unspent_experience_points)
   end
 end
--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -6,4 +6,16 @@ class Investigator < ApplicationRecord
   ] }
   validates :player_name, presence: true
   validates :deck_link, presence: true, format: URI::regexp(%w[http https])
+
+  validates :mental_trauma, presence: true
+  validates :physical_trauma, presence: true
+  validates :unspent_experience_points, presence: true
+
+  after_initialize :init
+
+  def init
+    self.mental_trauma ||= 0
+    self.physical_trauma ||= 0
+    self.unspent_experience_points ||= 0
+  end
 end
